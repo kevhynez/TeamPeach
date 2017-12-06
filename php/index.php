f6c9a48b358a62508c4a61246c694123e0a5be03
anthonymoore1991 <anthonymoore1991@gmail.com>
2017-12-06 09:05:14 -0500

    images added

--- php/index.php
+++ php/index.php
@@ -55,7 +55,6 @@ require 'config.php';
 		</div>
 	</nav>
 
-	<header>
 		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
 			<ol class="carousel-indicators">
 				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -64,21 +63,21 @@ require 'config.php';
 			</ol>
 			<div class="carousel-inner" role="listbox">
 				<!-- Slide One - Set the background image for this slide in the line below -->
-				<div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
+				<div class="carousel-item active" style="background-image: url('../images/hospitalimage.png')">
 					<div class="carousel-caption d-none d-md-block">
 						<h3>First Slide</h3>
 						<p>This is a description for the first slide.</p>
 					</div>
 				</div>
 				<!-- Slide Two - Set the background image for this slide in the line below -->
-				<div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+				<div class="carousel-item" style="background-image: url('../images/streetimage.JPG')">
 					<div class="carousel-caption d-none d-md-block">
 						<h3>Second Slide</h3>
 						<p>This is a description for the second slide.</p>
 					</div>
 				</div>
 				<!-- Slide Three - Set the background image for this slide in the line below -->
-				<div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+				<div class="carousel-item" style="background-image: url('../images/houseimage.jpg')">
 					<div class="carousel-caption d-none d-md-block">
 						<h3>Third Slide</h3>
 						<p>This is a description for the third slide.</p>
@@ -94,7 +93,6 @@ require 'config.php';
           <span class="sr-only">Next</span>
         </a>
 		</div>
-	</header>
 
 	<!-- Page Content -->
 	<div class="container">
